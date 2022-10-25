a22f7d095d120b49ad2fd4dce959d1201c5220fb
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2022-10-25 20:21:54 +0700

    Bài 219: Thêm Loading

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -15,7 +15,9 @@ const MoviePage = () => {
       setFilter(e.target.value);
    };
 
-   const { data } = useSWR(url, fetcher);
+   const { data, error } = useSWR(url, fetcher);
+
+   const loading = !data && !error;
 
    useEffect(() => {
       if (filterDebounce) {
@@ -58,9 +60,48 @@ const MoviePage = () => {
                </svg>
             </button>
          </div>
+         {loading && (
+            <div className="w-10 h-10 rounded-full border-4 border-primary border-t-transparent border-t-4 mx-auto an"></div>
+         )}
          <div className="grid grid-cols-4 gap-10">
-            {movies.length > 0 &&
+            {!loading &&
+               movies.length > 0 &&
                movies.map((item) => <MovieCard key={item.id} item={item} />)}
+         </div>
+         <div className="flex items-center justify-center text-white mt-10 gap-x-5">
+            <span className="cursor-pointer">
+               <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6">
+                  <path
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
+                     d="M15.75 19.5L8.25 12l7.5-7.5"
+                  />
+               </svg>
+            </span>
+            <span className="cursor-pointer inline-block py-2 px-4 rounded-md bg-white text-slate-900 leading-none">
+               1
+            </span>
+            <span className="cursor-pointer">
+               <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6">
+                  <path
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
+                     d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                  />
+               </svg>
+            </span>
          </div>
       </div>
    );
